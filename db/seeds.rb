4f2f689f5f8c609731dc98d4730848f0ea6de3a1
alterego-ip <danylo.burmei-ip231@nung.edu.ua>
2026-09-16 16:47:00 +0300

    feat: user auth and roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.find_or_create_by!(email: 'admin@store.com') do |user|
+  user.first_name = 'Адміністратор'
+  user.last_name = 'Системи'
+  user.password = 'password123'
+  user.password_confirmation = 'password123'
+  user.role = :admin
+end
+
+puts "Admin seed: admin@store.com / password123"
